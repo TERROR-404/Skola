bb9a406064f3e63a0d5e329616c8021fdefd4b56
Bohumil Čmerda <69761253+TERROR-404@users.noreply.github.com>
2024-10-07 20:52:44 +0200

    final

--- Skola/AppShell.xaml.cs
+++ Skola/AppShell.xaml.cs
@@ -6,8 +6,5 @@ public partial class AppShell : Shell
     {
         InitializeComponent();
         Routing.RegisterRoute(nameof(Views.AddTeacherPage), typeof(Views.AddTeacherPage));
-        Routing.RegisterRoute(nameof(Views.AddStudentPage), typeof(Views.AddStudentPage));
-        Routing.RegisterRoute(nameof(Views.AddMaintenancePage), typeof(Views.AddMaintenancePage));
-        Routing.RegisterRoute(nameof(Views.AddHelpPage), typeof(Views.AddHelpPage));
     }
 }
